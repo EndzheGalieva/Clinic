595edb19c62ef59b4a77dab486a670b98fb7d44c
Endzhe Galieva <endzhe91@gmail.com>
2021-08-09 21:53:55 +0300

    edit UserForm component template

--- src/components/UserForm/UserForm.js
+++ src/components/UserForm/UserForm.js
@@ -1,16 +1,87 @@
 import React, {Component} from 'react';
 import Grid from "@material-ui/core/Grid";
 import {FormControl} from "@material-ui/core";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import InputLabel from '@material-ui/core/InputLabel';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import Input from '@material-ui/core/Input';
+
+const groups = [
+  'VIP',
+  'Проблемные',
+  'ОМС',
+  'ДМС',
+];
 
 class UserForm extends Component {
   constructor(props) {
     super(props);
+    this.state = {personName: []}
   }
+
   render() {
     return (
       <Grid container>
         <Grid>
-          <FormControl/>
+          <FormControl>
+            <TextField
+              required
+              label="ФИО"
+              margin="normal"
+              name="fullName"
+            />
+            <TextField
+              required
+              label="Дата рождения"
+              margin="normal"
+              name="birthday"
+            />
+            <TextField
+              required
+              label="Номер телефона"
+              margin="normal"
+              name="phoneNumber"
+            />
+            <TextField
+              label="Пол"
+              margin="normal"
+              name="sex"
+            />
+            <FormControl>
+              <InputLabel required id="client-group-label">Группа клиентов</InputLabel>
+              <Select
+                labelId="client-group-label"
+                id="client-group"
+                multiple
+                value={this.state.personName}
+                input={<Input/>}
+              >
+                {groups.map((group) => (
+                  <MenuItem key={group} value={group}>
+                    {group}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            <FormControl>
+              <InputLabel id="doctor-select-label">Лечащий врач</InputLabel>
+              <Select
+                labelId="doctor-select-label"
+                id="doctor-select"
+              >
+                <MenuItem>Петров</MenuItem>
+                <MenuItem>Захаров</MenuItem>
+                <MenuItem>Черниговская</MenuItem>
+              </Select>
+            </FormControl>
+            <FormControlLabel
+              control={<Checkbox name="checkedA"/>}
+              label="Не отправлять СМС"
+            />
+          </FormControl>
         </Grid>
       </Grid>
     )
