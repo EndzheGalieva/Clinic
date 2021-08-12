fd579223f1cece93a73993dd4dde3cb0dcae53ef
Endzhe Galieva <endzhe91@gmail.com>
2021-08-12 21:34:00 +0300

    fix autocomplete

--- src/components/UserForm/UserForm.js
+++ src/components/UserForm/UserForm.js
@@ -1,24 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Grid from "@material-ui/core/Grid";
 import { Button, FormControl } from "@material-ui/core";
 import TextField from "@material-ui/core/TextField";
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import InputLabel from '@material-ui/core/InputLabel';
-import Select from '@material-ui/core/Select';
-import MenuItem from '@material-ui/core/MenuItem';
-import Input from '@material-ui/core/Input';
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import InputLabel from "@material-ui/core/InputLabel";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import Input from "@material-ui/core/Input";
 import styles from "../../styles";
-import axios from 'axios';
+import axios from "axios";
 import Autocomplete from "@material-ui/lab/Autocomplete";
 import PhoneMask from "./PhoneMask";
 
-const groups = [
-  'VIP',
-  'Проблемные',
-  'ОМС',
-  'ДМС',
-];
+const groups = ["VIP", "Проблемные", "ОМС", "ДМС"];
+
+const doctors = ["Петров", "Захаров", "Черниговская"];
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
@@ -26,58 +23,65 @@ const MenuProps = {
   PaperProps: {
     style: {
       maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250
-    }
-  }
+      width: 250,
+    },
+  },
 };
 
 class UserForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      fullName: '',
-      birthday: '',
-      phoneNumber: '',
-      gender: '',
+      fullName: "",
+      birthday: "",
+      phoneNumber: "",
+      gender: "",
       groupName: [],
-      doctor: '',
+      doctor: "",
       checkedSMS: false,
       errors: {},
       suggestions: [],
-    }
+    };
   }
 
-  handleChange = event => {
+  handleChange = (event) => {
     const data = event.target.value;
     const name = event.target.name;
-    if (name === "fullName") {
-      const url = 'https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/fio';
-      const token = '3ba950c928eb7a21e6e3c0eb084c287309d53f6b';
-      axios.post(url, { query: data }, {
-        headers: {
-          "Content-Type": "application/json",
-          "Accept": "application/json",
-          "Authorization": "Token " + token
-        }
-      }).then(res => this.setState({ suggestions: res.data.suggestions }))
-      this.setState({ ...this.state, [name]: data })
-      console.log(this.state)
-    } else {
-      this.setState({ ...this.state, [name]: data })
-      console.log(this.state)
-    }
+    this.setState({ ...this.state, [name]: data });
   };
 
-  handleCheckboxChange = event => {
+  handleFullNameChange = (event) => {
+    const data = event.target.value;
+    const url =
+      "https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/fio";
+    const token = "3ba950c928eb7a21e6e3c0eb084c287309d53f6b";
+    axios.post(
+      url,
+      { query: data },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          Authorization: "Token " + token,
+        },
+      }).then(res => this.setState({ suggestions: res.data.suggestions }));
+  };
+
+  handleAutocompleteChange = (event) => {
+    const text = event.target.outerText;
+    this.setState({ fullName: text });
+  };
+
+  handleCheckboxChange = (event) => {
     const checked = event.target.checked;
     this.setState({ checkedSMS: checked });
   };
 
-  handleDateChange = date => {
+  handleDateChange = (date) => {
     this.setState({ setSelectedDate: date });
   };
 
-  onSubmit = event => {
+  onSubmit = (event) => {
     event.preventDefault();
     const user = {
       fullName: this.state.fullName,
@@ -86,12 +90,10 @@ class UserForm extends Component {
       gender: this.state.gender,
       groupName: this.state.groupName,
       doctor: this.state.doctor,
-      checkedSMS: this.state.checkedSMS
+      checkedSMS: this.state.checkedSMS,
     };
     console.log(user);
   };
-
-
 
   render() {
     const { classes } = this.props;
@@ -106,21 +108,38 @@ class UserForm extends Component {
             <Autocomplete
               {...flatProps}
               id="flat-demo"
-              renderInput={(params) => <TextField required {...params} label="ФИО" name="fullName"
-                onChange={this.handleChange} helperText={errors.fullName} margin="normal" />}
+              name="autocomplete"
+              onChange={this.handleAutocompleteChange}
+              getOptionSelected={(option, value) => option.value === value.value}
+              renderInput={(params) => (
+                <TextField
+                  required
+                  name="fullName"
+                  onChange={this.handleFullNameChange}
+                  value={this.state.fullName}
+                  {...params}
+                  label="ФИО"
+                  helperText={errors.fullName}
+                  margin="normal"
+                />
+              )}
             />
             <TextField
               required
               id="date"
               label="Birthday"
               type="date"
+              name="birthday"
               className={classes.textField}
               InputLabelProps={{
                 shrink: true,
               }}
+              onChange={this.handleChange}
             />
             <FormControl>
-              <InputLabel required htmlFor="phone-number-mask-input">Номер телефона</InputLabel>
+              <InputLabel required htmlFor="phone-number-mask-input">
+                Номер телефона
+              </InputLabel>
               <Input
                 value={this.state.phoneNumber}
                 onChange={this.handleChange}
@@ -136,7 +155,9 @@ class UserForm extends Component {
               onChange={this.handleChange}
             />
             <FormControl>
-              <InputLabel required id="client-group-label">Группа клиентов</InputLabel>
+              <InputLabel required id="client-group-label">
+                Группа клиентов
+              </InputLabel>
               <Select
                 labelId="client-group-label"
                 id="client-group"
@@ -154,31 +175,37 @@ class UserForm extends Component {
                 ))}
               </Select>
             </FormControl>
-            
             <FormControl>
               <InputLabel id="doctor-select-label">Лечащий врач</InputLabel>
               <Select
                 labelId="doctor-select-label"
                 id="doctor-select"
                 name="doctor"
+                onChange={this.handleChange}
+                value={this.state.doctor}
               >
-                <MenuItem>Петров</MenuItem>
-                <MenuItem>Захаров</MenuItem>
-                <MenuItem>Черниговская</MenuItem>
+                {doctors.map((name) => (
+                  <MenuItem key={name} value={name}>
+                    {name}
+                  </MenuItem>
+                ))}
               </Select>
             </FormControl>
             <FormControlLabel
-              control={<Checkbox name="checkedSMS" onChange={this.handleCheckboxChange} />}
+              control={
+                <Checkbox
+                  name="checkedSMS"
+                  onChange={this.handleCheckboxChange}
+                />
+              }
               label="Не отправлять СМС"
             />
-            <Button onClick={this.onSubmit}>
-              Создать
-            </Button>
+            <Button onClick={this.onSubmit}>Создать</Button>
           </FormControl>
         </Grid>
       </Grid>
-    )
+    );
   }
 }
 
-export default (styles(UserForm));
+export default styles(UserForm);
